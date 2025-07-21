56920dbf9ef312632612bf34d2a7b5ec39eb4bec
Alexis <alexis.balzano@epfl.ch>
2025-07-21 19:44:01 +0200

    Correction of color definition

--- src/config/Colors.h
+++ src/config/Colors.h
@@ -3,8 +3,8 @@
 #include <optional>
 
 namespace Colors{
-	std::optional<std::array<unsigned int, 3>> green = std::make_optional<std::array<unsigned int, 3>>({ 127, 252, 73 });
-	std::optional<std::array<unsigned int, 3>> white = std::make_optional<std::array<unsigned int, 3>>({ 255, 255, 255});
-	std::optional<std::array<unsigned int, 3>> red = std::make_optional<std::array<unsigned int, 3>>({ 240, 0, 0 });
-	std::optional<std::array<unsigned int, 3>> orange = std::make_optional<std::array<unsigned int, 3>>({ 255, 153, 51 });
+	std::optional<std::array<unsigned int, 3>> green = std::array<unsigned int, 3>{127, 252, 73};
+	std::optional<std::array<unsigned int, 3>> white = std::array<unsigned int, 3>({ 255, 255, 255});
+	std::optional<std::array<unsigned int, 3>> red = std::array<unsigned int, 3>({ 240, 0, 0 });
+	std::optional<std::array<unsigned int, 3>> orange = std::array<unsigned int, 3>({ 255, 153, 51 });
 }
