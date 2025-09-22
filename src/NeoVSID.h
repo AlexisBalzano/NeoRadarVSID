324ffa43fbca4f8e756963225f3e3991c20747ce
Alexis <alexis.balzano@epfl.ch>
2025-09-22 13:00:09 +0200

    Switched to v1.3.9

--- src/NeoVSID.h
+++ src/NeoVSID.h
@@ -8,7 +8,7 @@
 #include "core/DataManager.h"
 #include "utils/Color.h"
 
-constexpr const char* NEOVSID_VERSION = "v1.3.8";
+constexpr const char* NEOVSID_VERSION = "v1.3.9";
 
 using namespace PluginSDK;
 
